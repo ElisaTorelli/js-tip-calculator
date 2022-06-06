c4516d1bac4b98e5a8a8f763275e414584254356
ElisaTorelli <elisa.torelli1@gmail.com>
2022-06-06 17:26:58 +0200

    add code snippet

---
+++ main.js
@@ -0,0 +1,19 @@
+//USER INPUT FORMS
+const billUserInput = document.querySelector(".bill__input");
+const tipsOptionsUserInput = document.querySelectorAll(".tips");
+const peopleUserInput = document.querySelector(".people__number");
+//TOTAL AMOUNT & TIP AMOUNT
+const tipAmount = document.querySelector(".tip__amount");
+const totalPriceAmount = document.querySelector(".price__amount");
+
+
+let billDefaultValue = 0.0;
+let peopleDefaultValue = 1;
+let tipCustomDefaultValue = 0.0;
+
+
+//RESET BTN & ERROR MESSAGE
+const resetButton = document.querySelector(".reset__btn");
+const errorMessage = document.querySelector(".error");
+
+
